fa17802d115a55dbad36e908e115a39386bd0985
skazkin <russkazkin@gmail.com>
2020-03-05 20:15:43 +0300

    Add lesson 12 code

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -7,6 +7,7 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -14,11 +15,27 @@
     <title>Todos</title>
 </head>
 <body>
-    <h1>TODOS PAGE</h1>
-    <ul>
-    @foreach($todos as $todo)
-        <li>{{ $todo->name }}</li>
-    @endforeach
-    </ul>
+    <div class="container my-5">
+        <h1>TODOS PAGE</h1>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        Todos
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                        @foreach($todos as $todo)
+                            <li class="list-group-item">
+                                {{ $todo->name }}
+                                <div class="btn btn-primary btn-sm float-right">View</div>
+                            </li>
+                        @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
