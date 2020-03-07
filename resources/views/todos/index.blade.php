a12bd8bcca9f23d49e574f6d3ca24c4520e839da
skazkin <russkazkin@gmail.com>
2020-03-07 13:44:42 +0300

    Add lesson 14 code

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,41 +1,28 @@
-<?php
-/**
- * @var $todos \App\Todo[]
- */
-?>
+@extends('layouts.app')
 
-<!doctype html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Todos</title>
-</head>
-<body>
-    <div class="container my-5">
-        <h1>TODOS PAGE</h1>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        Todos
-                    </div>
-                    <div class="card-body">
-                        <ul class="list-group">
-                        @foreach($todos as $todo)
-                            <li class="list-group-item">
-                                {{ $todo->name }}
-                                <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
-                            </li>
-                        @endforeach
-                        </ul>
-                    </div>
-                </div>
+@section('title')
+Todos List
+@endsection
+
+@section('content')
+<h1 class="text-center my-5">TODOS PAGE</h1>
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">
+                Todos
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    @foreach($todos as $todo)
+                        <li class="list-group-item">
+                            {{ $todo->name }}
+                            <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
-</body>
-</html>
+</div>
+@endsection
