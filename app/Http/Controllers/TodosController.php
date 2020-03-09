4e8a08adbf8ada6a10f451c5f973afba13bcf83f
skazkin <russkazkin@gmail.com>
2020-03-09 19:03:41 +0300

    Complete lesson 20

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -36,6 +36,8 @@ class TodosController extends Controller
         $todo->completed = false;
         $todo->save();
 
+        session()->flash('success', 'Todo created successfully.');
+
         return redirect('/todos');
     }
 
@@ -57,13 +59,16 @@ class TodosController extends Controller
         $todo->description = $data['description'];
         $todo->save();
 
+        session()->flash('success', 'Todo updated successfully.');
+
         return redirect('/todos');
     }
 
     public function delete(Todo $todo)
     {
-
         $todo->delete();
+
+        session()->flash('success', 'Todo deleted successfully.');
 
         return redirect('/todos');
     }
